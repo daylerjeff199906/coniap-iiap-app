9d31789a5204796ace81f2f133239134755bc1d7
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2024-03-07 00:06:04 -0500

    add sections

--- src/pages/home/aboutUsSection.tsx
+++ src/pages/home/aboutUsSection.tsx
@@ -2,12 +2,21 @@ export const AboutUsSection = () => {
   return (
     <section className=" bg-white">
       <div className="container section-home">
-        <h2>About Us</h2>
-        <p>
-          We are a team of professionals who are passionate about helping
-          people. We are dedicated to providing the best service to our
-          customers.
+        <h2 className="title-section-home">Acerca del congreso</h2>
+        <p className="pt-2">
+          El III Congreso Internacional sobre la Amazonía Peruana (CIIAP 2024)
+          es un espacio internacional para la reflexión académica y práctica, en
+          el cual se abordarán temas relacionados con la investigación para el
+          desarrollo, perspectivas y retos de la Amazonía peruana. En ese
+          sentido, invitamos a investigadores, profesionales y estudiantes a
+          presentar trabajos de investigación en las áreas temáticas del
+          congreso. Es importante destacar que los idiomas del congreso son
+          español e inglés, asegurando un ambiente diverso e inclusivo para
+          todos los participantes.
         </p>
+        <h3 className="pt-4 font-bold text-lg">
+          Informaciòn para los participantes
+        </h3>
       </div>
     </section>
   )
