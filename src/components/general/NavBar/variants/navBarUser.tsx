569e3897a0b46b2fe411ae0ba0884d7797456809
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2024-03-07 23:51:21 -0500

    fixed sections

--- src/components/general/NavBar/variants/navBarUser.tsx
+++ src/components/general/NavBar/variants/navBarUser.tsx
@@ -1,12 +1,27 @@
+'use client'
+import { useState } from 'react'
 import {
+  Button,
   Navbar,
   NavbarBrand,
   NavbarContent,
   NavbarItem,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle,
 } from '@nextui-org/react'
 import Image from 'next/image'
+import Link from 'next/link'
+
+const menuItems = [
+  { name: 'Home', link: '/' },
+  { name: 'Agenda', link: '/agenda' },
+  { name: 'Ponentes', link: '/ponentes' },
+]
 
 export const NavBarUser = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
   return (
     <>
       <Navbar
@@ -18,20 +33,57 @@ export const NavBarUser = () => {
         }}
         height={72}
       >
-        <NavbarBrand>
-          <Image
-            src="/logo_coniap.webp"
-            alt="Logo"
-            width={140}
-            height={100}
-            priority
-          />
-        </NavbarBrand>
         <NavbarContent>
-          <NavbarItem>Home</NavbarItem>
-          <NavbarItem>About</NavbarItem>
-          <NavbarItem>Contact</NavbarItem>
+          <NavbarMenuToggle
+            aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+            className="sm:hidden"
+          />
+          <NavbarBrand>
+            <Link href={'/'}>
+              <Image
+                src="/logo_coniap.webp"
+                alt="Logo"
+                width={140}
+                height={100}
+                priority
+              />
+            </Link>
+          </NavbarBrand>
         </NavbarContent>
+
+        <NavbarContent
+          className="hidden sm:flex gap-4"
+          justify="center"
+        >
+          {menuItems.map((item, index) => (
+            <NavbarItem key={index}>
+              <Link href={item.link}>{item.name}</Link>
+            </NavbarItem>
+          ))}
+        </NavbarContent>
+        <NavbarContent justify="end">
+          {/* <NavbarItem className="hidden lg:flex">
+            <Link href="#">Login</Link>
+          </NavbarItem> */}
+          <NavbarItem>
+            <Button
+              as={Link}
+              color="success"
+              href="#"
+              variant="bordered"
+              radius="full"
+            >
+              Inscribirme
+            </Button>
+          </NavbarItem>
+        </NavbarContent>
+        <NavbarMenu>
+          {menuItems?.map((item, index) => (
+            <NavbarMenuItem key={index}>
+              <Link href={item.link}>{item.name}</Link>
+            </NavbarMenuItem>
+          ))}
+        </NavbarMenu>
       </Navbar>
     </>
   )
