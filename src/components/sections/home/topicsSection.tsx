336a9e8dfafdd8407c23d4f93b05879adffc68c7
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2024-03-09 08:33:54 -0500

    fixed section topics

--- src/components/sections/home/topicsSection.tsx
+++ src/components/sections/home/topicsSection.tsx
@@ -1,42 +1,60 @@
 import { Card } from '@nextui-org/react'
+import {
+  IconFish,
+  IconTrees,
+  IconPlant,
+  IconSettings,
+  IconBrain,
+} from '@tabler/icons-react'
 
 const topicsExample = [
   {
     title:
       'EL AGUA Y SUS RECURSOS EN LA AMAZONIA: HIDROBIOLOGIA, ACUICULTURA Y PESCA',
-    description:
-      'Explora la gestión de recursos hídricos, calidad del agua y su impacto en la vida acuática y las comunidades ribereñas en la región amazónica.',
-    image: 'https://via.placeholder.com/150',
-    color: 'bg-danger-500/75',
+    icon: (
+      <IconFish
+        size={60}
+        stroke={1}
+      />
+    ),
   },
   {
     title: 'ECOLOGiA Y USO SOSTENIBLE DE BOSQUES AMAZÓNICOS',
-    description:
-      'Estudia la biología de organismos acuáticos, el cultivo de especies nativas y la pesca sostenible para el desarrollo económico y alimentario en la Amazonia.',
-    image: 'https://via.placeholder.com/150',
-    color: 'bg-primary-500/75',
+    icon: (
+      <IconTrees
+        size={60}
+        stroke={1}
+      />
+    ),
   },
   {
     title: 'BIODIVERSIDAD',
-    description:
-      'Analiza la variedad de especies de flora y fauna en la Amazonia, su importancia ecológica y los esfuerzos de conservación para proteger este rico patrimonio biológico.',
-    image: 'https://via.placeholder.com/150',
-    color: 'bg-success-500/75',
+    icon: (
+      <IconPlant
+        size={60}
+        stroke={1}
+      />
+    ),
   },
   {
     title:
       'CONOCIMIENTOS TRADICIONALES, CONSERVACIÓN Y BUEN VIVIR EN LA AMAZONIA PERUANA',
-    description:
-      'Examina las prácticas ancestrales de las comunidades indígenas para la conservación de la biodiversidad y su relación con el bienestar social y cultural en la región.',
-    image: 'https://via.placeholder.com/150',
-    color: 'bg-warning-500/75',
+
+    icon: (
+      <IconSettings
+        size={60}
+        stroke={1}
+      />
+    ),
   },
   {
     title: 'GESTIÓN TERRITORIAL PARA EL DESARROLLO DE LA AMAZONÍA PERUANA',
-    description:
-      'Considera las políticas y estrategias de planificación territorial que promueven un desarrollo sostenible, equitativo y respetuoso con el medio ambiente en la Amazonía peruana.',
-    image: 'https://via.placeholder.com/150',
-    color: 'bg-secondary-500/75',
+    icon: (
+      <IconBrain
+        size={60}
+        stroke={1}
+      />
+    ),
   },
 ]
 
@@ -52,9 +70,7 @@ export const TopicsSection = () => {
             <CardTopics
               key={i}
               title={topic.title}
-              description={topic.description}
-              image={topic.image}
-              color={topic.color}
+              icon={topic.icon}
             />
           ))}
         </div>
@@ -63,24 +79,16 @@ export const TopicsSection = () => {
   )
 }
 
-const CardTopics = ({
-  title,
-  description,
-  image,
-  color,
-}: {
-  title: string
-  description: string
-  image: string
-  color: string
-}) => {
+const CardTopics = ({ title, icon }: { title: string; icon: JSX.Element }) => {
   return (
     <Card
-      className={` ${color} text-white p-6 text-center flex flex-col justify-center items-center gap-2 animate-appearance-in `}
+      className={`bg-transparent text-white p-6 text-center flex flex-col justify-center items-center gap-2 animate-appearance-in `}
       radius="none"
     >
+      <div className="flex justify-center items-center  rounded-full p-4">
+        {icon}
+      </div>
       <h3 className="font-bold">{title}</h3>
-      <p className="text-sm line-clamp-2">{description}</p>
     </Card>
   )
 }
