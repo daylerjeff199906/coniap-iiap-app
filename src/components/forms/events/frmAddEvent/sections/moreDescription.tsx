2a401203bab74be5e2cf4b54cf1c8ca8e94f594a
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2024-03-17 11:27:45 -0500

    fixed editor

--- src/components/forms/events/frmAddEvent/sections/moreDescription.tsx
+++ src/components/forms/events/frmAddEvent/sections/moreDescription.tsx
@@ -1,6 +1,8 @@
 'use client'
-import useQuill from './useQuill'
 import { useFormContext } from 'react-hook-form'
+
+import { useQuill } from 'react-quilljs'
+import 'quill/dist/quill.snow.css' // Add css for snow theme
 
 export const MoreDescription = () => {
   const { setValue } = useFormContext()
@@ -9,11 +11,18 @@ export const MoreDescription = () => {
     // Manejar cambios en la descripción aquí
     setValue('customContent', content)
   }
+  const { quillRef } = useQuill({
+    onchange: handleDescriptionChange,
+  })
 
   return (
     <section className="grid grid-cols-1 gap-4">
       <h1 className="text-lg">Personalizar contenido</h1>
-      {useQuill({ onChange: handleDescriptionChange })}
+      {/* {useQuill({ onChange: handleDescriptionChange })} */}
+      <div
+        style={{ height: '260px' }}
+        ref={quillRef}
+      />
     </section>
   )
 }
