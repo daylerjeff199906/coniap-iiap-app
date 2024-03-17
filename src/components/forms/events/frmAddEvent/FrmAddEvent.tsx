a3b33c84b1166ff63cd0ff3e1ed908cef773c6a5
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2024-03-16 23:13:25 -0500

    add react form hook

--- src/components/forms/events/frmAddEvent/FrmAddEvent.tsx
+++ src/components/forms/events/frmAddEvent/FrmAddEvent.tsx
@@ -1,22 +1,35 @@
 'use client'
 import { Button } from '@nextui-org/react'
+import { useForm, FormProvider, SubmitHandler } from 'react-hook-form'
+
 import { InfoGeneral, MoreDescription, MoreInfo } from './sections'
+import { IEvent } from '@/types'
 
 export const FrmAddEvent = () => {
+  const methods = useForm<IEvent>()
+  const onSubmit: SubmitHandler<IEvent> = (data) => {
+    console.log(data)
+  }
+
   return (
     <>
-      <form className="space-y-3">
-        <h1 className="text-2xl font-bold">Agregar Evento</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-5">
-          <InfoGeneral />
-          <MoreInfo />
-        </div>
-        <MoreDescription />
-        <div className="flex items-center gap-4">
-          <Button color="primary">Agregar evento</Button>
-          <Button>Cancelar</Button>
-        </div>
-      </form>
+      <FormProvider {...methods}>
+        <form
+          className="space-y-3"
+          onSubmit={methods.handleSubmit(onSubmit)}
+        >
+          <h1 className="text-2xl font-bold">Agregar Evento</h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-5">
+            <InfoGeneral />
+            <MoreInfo />
+          </div>
+          <MoreDescription />
+          <div className="flex items-center gap-4">
+            <Button color="primary">Agregar evento</Button>
+            <Button>Cancelar</Button>
+          </div>
+        </form>
+      </FormProvider>
     </>
   )
 }
