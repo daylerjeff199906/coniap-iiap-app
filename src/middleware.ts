73f4a1b1e211bc6579de269ce42f6180b01979b9
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2024-03-19 22:44:38 -0500

    add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  const currentUser = request.cookies.get('currentUser')?.value
+
+  if (currentUser) {
+    return NextResponse.redirect(new URL('/', request.url))
+  }
+  return NextResponse.redirect(new URL('/login', request.url))
+}
+
+export const config = {
+  //   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  matcher: '/admin',
+}
