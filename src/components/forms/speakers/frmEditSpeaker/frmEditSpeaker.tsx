538d87075fe16581c10ef97b0a62369df92eac8c
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2024-03-20 15:17:29 -0500

    add hooks

--- src/components/forms/speakers/frmEditSpeaker/frmEditSpeaker.tsx
+++ src/components/forms/speakers/frmEditSpeaker/frmEditSpeaker.tsx
@@ -3,6 +3,9 @@ import { useState } from 'react'
 import { ISpeaker } from '@/types'
 import { useForm, FormProvider, SubmitHandler } from 'react-hook-form'
 import { InfoGeneralSection, MultimediasSection } from './sections'
+import { Button } from '@nextui-org/react'
+import Link from 'next/link'
+import { ModalAction } from '@/components'
 interface IProps {
   speaker: ISpeaker
 }
@@ -10,28 +13,61 @@ interface IProps {
 export const FrmEditSpeaker = (props: IProps) => {
   const { speaker } = props
   const [isEditables, setIsEditables] = useState(true)
+  const [openConfirm, setOpenConfirm] = useState(false)
 
   const methods = useForm<ISpeaker>({
     defaultValues: speaker,
   })
 
+  const onSubmit = () => {
+    setOpenConfirm(true)
+  }
+
+  const handleSave: SubmitHandler<ISpeaker> = (data: ISpeaker) => {
+    console.log(data)
+  }
+
   return (
     <>
-      <header className='pb-4'>
+      <header className="pb-4">
         <h2 className="text-2xl font-bold">
           {isEditables ? 'Editar ponente' : 'Detalles del ponente'}
         </h2>
       </header>
       <FormProvider {...methods}>
-        <form className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+        <form
+          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 sm:gap-4 lg:gap-5"
+          onSubmit={methods.handleSubmit(onSubmit)}
+        >
           <div className="w-full col-span-1">
             <MultimediasSection />
           </div>
           <div className="w-full col-span-1 lg:col-span-2">
             <InfoGeneralSection />
           </div>
+          <footer className="flex items-center gap-3 justify-end col-span-1 sm:col-span-2 lg:col-span-3">
+            <Button
+              color="primary"
+              type="submit"
+            >
+              Guardar
+            </Button>
+            <Button
+              as={Link}
+              href="/admin/ponentes"
+            >
+              Cancelar
+            </Button>
+          </footer>
         </form>
       </FormProvider>
+      <ModalAction
+        isOpen={openConfirm}
+        message="¿Estás seguro de guardar los cambios?"
+        onPress={methods.handleSubmit(handleSave)}
+        setOpen={setOpenConfirm}
+        title="Guardar cambios"
+      />
     </>
   )
 }
--- src/components/forms/speakers/frmEditSpeaker/sections/multimediaSection.tsx
+++ src/components/forms/speakers/frmEditSpeaker/sections/multimediaSection.tsx
@@ -1,13 +1,26 @@
+import { useState } from 'react'
 import { Image } from '@nextui-org/react'
 import { useFormContext } from 'react-hook-form'
+import { useFiles } from '@/hooks/admin'
+
+import { FilePond } from 'react-filepond'
 
 export const MultimediasSection = () => {
-  const { watch } = useFormContext()
+  const { watch, setValue } = useFormContext()
+  const { uploadImage, editField } = useFiles()
+
+  const [files, setFiles] = useState([])
 
   const urlImage = watch('image')
+  const id = watch('id')
+
+  const handleUpdateFiles = (fileItems: any) => {
+    // Set current file objects to this.state
+    setFiles(fileItems.map((fileItem: any) => fileItem.file))
+  }
   return (
     <>
-      <section className="w-full">
+      <section className="w-full flex flex-col gap-3">
         <div className="flex items-center justify-center w-full">
           <Image
             src={urlImage || 'https://via.placeholder.com/150'}
@@ -16,6 +29,37 @@ export const MultimediasSection = () => {
             removeWrapper
           />
         </div>
+        <FilePond
+          allowMultiple={false}
+          acceptedFileTypes={['image/*']}
+          labelIdle='Arrastra y suelta tu imagen o <span class="filepond--label-action"> busca </span>'
+          server={{
+            process: async (
+              fieldName,
+              file,
+              metadata,
+              load,
+              error,
+              progress,
+              abort
+            ) => {
+              try {
+                const data = file as File
+                const url = await uploadImage('speakers', data)
+                if (url !== '') {
+                  await editField(id, 'speakers', 'banner', url)
+                  setValue('image', url)
+                }
+                load(url)
+              } catch (error) {
+                console.error('Upload error', error)
+              }
+            },
+          }}
+          files={files}
+          onupdatefiles={handleUpdateFiles}
+          instantUpload={false}
+        />
       </section>
     </>
   )
