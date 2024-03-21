36c63564f72e28ea04f2f3fabef6e497079a0257
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2024-03-21 18:16:36 -0500

    fixed use sponsors admin

--- src/components/sections/admin/sponsors/listSponsors.tsx
+++ src/components/sections/admin/sponsors/listSponsors.tsx
@@ -3,23 +3,13 @@
 import { TableGeneral } from '@/components'
 import { IColumns } from '@/types'
 
-import { useSpeakers } from '@/hooks/admin'
+import { useSponsors } from '@/hooks/admin'
 import { useEffect } from 'react'
 
 const columns: Array<IColumns> = [
   {
-    key: 'level',
-    label: 'Nivel de estudio',
-    align: 'start',
-  },
-  {
     key: 'name',
     label: 'Ponente',
-    align: 'start',
-  },
-  {
-    key: 'job',
-    label: 'Puesto de trabajo',
     align: 'start',
   },
   {
@@ -39,14 +29,14 @@ const columns: Array<IColumns> = [
   },
 ]
 export const ListSponsorsSections = () => {
-  const { getSpekers, speakers, editSpeakerField, loading } = useSpeakers()
+  const { getSponsors, sponsors, loading } = useSponsors()
 
   // const searchParams = useSearchParams()
 
   // const isEdit = searchParams.get('edit') !== null
 
   useEffect(() => {
-    getSpekers()
+    getSponsors()
   }, [])
 
   // useEffect(() => {
@@ -65,29 +55,26 @@ export const ListSponsorsSections = () => {
   //   fetchData()
   // }, [event, isEdit])
 
-  const handleStatusChange = async (key: string, value: boolean) => {
-    await editSpeakerField(key, 'isActive', value)
-    getSpekers()
-  }
+  // const handleStatusChange = async (key: string, value: boolean) => {
+  //   await editSpeakerField(key, 'isActive', value)
+  //   getSpekers()
+  // }
 
   return (
     <>
       <TableGeneral
         loading={loading}
         columns={columns}
-        onValueStatusChange={(key: string | number, value: boolean) => {
-          handleStatusChange(String(key), value)
-        }}
+        // onValueStatusChange={(key: string | number, value: boolean) => {
+        //   handleStatusChange(String(key), value)
+        // }}
         rows={
-          speakers
-            ? speakers?.map((speaker) => {
+          sponsors
+            ? sponsors?.map((sponsor) => {
                 return {
-                  key: speaker.id,
-                  name: RenderColumnName(speaker.fullName, speaker.surName),
-                  job: speaker.job,
-                  institution: speaker.institution,
-                  level: speaker.levelStudy,
-                  status: speaker.isActive,
+                  key: sponsor.id,
+                  name: sponsor.name,
+                  status: sponsor.isActive,
                   actions: 'actions',
                 }
               })
@@ -98,11 +85,11 @@ export const ListSponsorsSections = () => {
   )
 }
 
-const RenderColumnName = (fullname: string, surname: string) => {
-  return (
-    <div className="flex flex-col">
-      <p className="text-base font-bold">{fullname}</p>
-      <p className="text-xs font-medium text-slate-500">{surname}</p>
-    </div>
-  )
-}
+// const RenderColumnName = (fullname: string, surname: string) => {
+//   return (
+//     <div className="flex flex-col">
+//       <p className="text-base font-bold">{fullname}</p>
+//       <p className="text-xs font-medium text-slate-500">{surname}</p>
+//     </div>
+//   )
+// }
