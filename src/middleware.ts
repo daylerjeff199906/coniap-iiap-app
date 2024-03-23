c4c159f357f2c9e64c53ce30d088c3881e5accb4
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2024-03-23 01:26:23 -0500

    fixed files

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,15 @@
+import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
   const currentUser = request.cookies.get('user')?.value
 
-  if (currentUser && !request.nextUrl.pathname.startsWith('/admin')) {
-    return Response.redirect(new URL('/admin', request.url))
+  if (!currentUser && request.nextUrl.pathname.startsWith('/admin')) {
+    return NextResponse.redirect(new URL('/login', request.url))
   }
-
-  if (!currentUser && !request.nextUrl.pathname.startsWith('/login')) {
-    return Response.redirect(new URL('/login', request.url))
-  }
+  return NextResponse.next()
 }
+
 export const config = {
   //   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
   matcher: ['/admin/:path*'],
