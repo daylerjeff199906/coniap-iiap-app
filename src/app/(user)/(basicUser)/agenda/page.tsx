75b34d972f9753a92f6cc33f864861e380131512
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2024-03-22 21:31:13 -0500

    fixed pages programs

--- src/app/(user)/(basicUser)/agenda/page.tsx
+++ src/app/(user)/(basicUser)/agenda/page.tsx
@@ -1,7 +1,31 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+'use client'
+import { useEffect } from 'react'
+import { LoadingPages } from '@/components'
+import { usePrograms } from '@/hooks/client'
+import { DataNotFound } from '@/components'
+
 export default function Page() {
+  const { getPrograms, programs, loading } = usePrograms()
+
+  useEffect(() => {
+    getPrograms()
+  }, [])
+
   return (
     <>
-      <section className="section-page">Page</section>
+      <main className="container">
+        {programs !== null && programs.length > 0 ? (
+          <>
+            <section className="py-4">
+              <h1>Eventos</h1>
+            </section>
+          </>
+        ) : (
+          <DataNotFound />
+        )}
+      </main>
+      <LoadingPages isOpen={loading} />
     </>
   )
 }
