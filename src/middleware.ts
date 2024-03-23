055770a1c442ffeddf8714dbb8619df37c76cbe8
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2024-03-23 01:32:57 -0500

    fixed middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,11 +3,14 @@ import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
   const currentUser = request.cookies.get('user')?.value
+    ? JSON.parse(request.cookies.get('user')?.value as string)
+    : null
 
   if (!currentUser && request.nextUrl.pathname.startsWith('/admin')) {
     return NextResponse.redirect(new URL('/login', request.url))
+  } else {
+    return NextResponse.next()
   }
-  return NextResponse.next()
 }
 
 export const config = {
