832083eec37f8a9ef598d40e86f6fa633bb6d5fe
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2024-03-25 22:18:34 -0500

    delete files

--- src/components/sections/ponentes/listSpeakers.tsx
+++ src/components/sections/ponentes/listSpeakers.tsx
@@ -1,44 +1,49 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 'use client'
-import { useEffect } from 'react'
-import { motion } from 'framer-motion'
+// import { motion } from 'framer-motion'
 
-import { useSpeaker } from '@/hooks/client'
 import { CardSpeaker, DataNotFound } from '@/components'
 
-import svgImage from '@/assets/svg/ISOTIPO - CONIAP.svg'
+// import svgImage from '@/assets/svg/ISOTIPO - CONIAP.svg'
+import { IPerson } from '@/types'
 
-const icon = {
-  hidden: {
-    opacity: 0,
-    pathLength: 0,
-    fill: 'rgba(255, 255, 255, 0)',
-  },
-  visible: {
-    opacity: 1,
-    pathLength: 1,
-    fill: 'rgba(255, 255, 255, 1)',
-  },
+// const icon = {
+//   hidden: {
+//     opacity: 0,
+//     pathLength: 0,
+//     fill: 'rgba(255, 255, 255, 0)',
+//   },
+//   visible: {
+//     opacity: 1,
+//     pathLength: 1,
+//     fill: 'rgba(255, 255, 255, 1)',
+//   },
+// }
+
+interface IProps {
+  speakers: IPerson[] | undefined
 }
 
-export const ListSpeakers = () => {
-  const { getSpekersActive, speakersActive, loading } = useSpeaker()
-
-  useEffect(() => {
-    getSpekersActive()
-  }, [])
+export const ListSpeakers = (props: IProps) => {
+  const { speakers } = props
 
   return (
     <>
-      {speakersActive && speakersActive.length > 0 ? (
-        <div className="w-full grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2 sm:gap-3 lg:gap-4">
-          {speakersActive?.map((speaker, index) => (
-            <CardSpeaker
-              speaker={speaker}
-              key={index}
-            />
-          ))}
-        </div>
+      {speakers !== undefined ? (
+        <>
+          {speakers && speakers.length > 0 ? (
+            <div className="w-full grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2 sm:gap-3 lg:gap-4">
+              {speakers.map((speaker, index) => (
+                <CardSpeaker
+                  speaker={speaker}
+                  key={index}
+                />
+              ))}
+            </div>
+          ) : (
+            <DataNotFound />
+          )}
+        </>
       ) : (
         <DataNotFound />
       )}
