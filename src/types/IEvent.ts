aec8a121dce3d2929209e5724aad106fe9585310
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2024-03-25 21:07:41 -0500

    fixed types

--- src/types/IEvent.ts
+++ src/types/IEvent.ts
@@ -1,19 +1,21 @@
+import { IPerson } from '.'
 export interface IEvent {
   id: string
+  //observar
+  person: IPerson | null
   isActived: boolean
   name: string
   timeStart: string
   timeEnd: string
   date?: string
   shortDescription?: string
-  place: string
   banner?: string
-  images: string[]
-  salaId: string
   linkZoom?: string
   linkYoutube?: string
   linkFacebook?: string
+  salaId: string
   customContent?: string
+  //delete
   idProgram?: string
   inProgram?: boolean
   idTypeEvent?: string
