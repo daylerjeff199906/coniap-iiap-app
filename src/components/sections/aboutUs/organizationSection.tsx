14ac68a201cf854f6abc2eccacaf03586664c005
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2024-03-27 16:29:54 -0500

    fixed about section

--- src/components/sections/aboutUs/organizationSection.tsx
+++ src/components/sections/aboutUs/organizationSection.tsx
@@ -1,10 +1,13 @@
 'use client'
+import { Accordion, AccordionItem, Image } from '@nextui-org/react'
 import { motion } from 'framer-motion'
 import { useInView } from 'react-intersection-observer'
+import { IconPlus } from '@tabler/icons-react'
+import svgAddorm from '@/assets/svg/patron-fino.svg'
 
 const data = [
   {
-    id: 1,
+    id: '1',
     type: 'COMITÉ ORGANIZADOR',
     persons: [
       'Carmen García (Coordinador del Evento)',
@@ -17,7 +20,7 @@ const data = [
     ],
   },
   {
-    id: 2,
+    id: '2',
     type: 'COMITÉ CIENTÍFICO',
     persons: [
       'Carmen García',
@@ -39,7 +42,7 @@ const data = [
     ],
   },
   {
-    id: 3,
+    id: '3',
     type: 'COMITÉ DE INFORMÁTICA Y DIFUSIÓN',
     persons: [
       '  Américo Sánchez (Coordinador de Informática)',
@@ -60,7 +63,7 @@ export const OrganizationSection = () => {
   return (
     <>
       <section
-        className=""
+        className="py-10 sm:py-20"
         ref={ref}
       >
         <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 sm:gap-5">
@@ -82,8 +85,41 @@ export const OrganizationSection = () => {
             >
               Comité: la <b>columna</b> vertebral del <b>CONIAP</b>
             </motion.h2>
+            <Image
+              src={svgAddorm.src}
+              alt="Patrón fino"
+              removeWrapper
+              className="w-full max-w-lg"
+            />
           </div>
-          <div></div>
+          <div className="sm:p-4 lg:p-6">
+            <Accordion
+              isCompact
+              defaultExpandedKeys={['1']}
+            >
+              {data?.map((item) => (
+                <AccordionItem
+                  key={item.id}
+                  title={item.type}
+                  classNames={{
+                    title: 'font-semibold underline text-primary-500',
+                  }}
+                  indicator={
+                    <IconPlus
+                      size={20}
+                      className="text-primary-500"
+                    />
+                  }
+                >
+                  <ul className="list-disc">
+                    {item.persons.map((person, index) => (
+                      <li key={index}>{person}</li>
+                    ))}
+                  </ul>
+                </AccordionItem>
+              ))}
+            </Accordion>
+          </div>
         </div>
       </section>
     </>
