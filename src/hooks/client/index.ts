ec91ab381f7a07bbb9c34f047c5e6d18d2be0135
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2024-03-28 09:43:44 -0500

    delete files

--- src/hooks/client/index.ts
+++ src/hooks/client/index.ts
@@ -1,5 +1,3 @@
 import { useEvents } from './useEvents'
-import { usePrograms } from './usePrograms'
-import { useTopics } from './useTopics'
 
-export { useEvents, usePrograms, useTopics }
+export { useEvents }
