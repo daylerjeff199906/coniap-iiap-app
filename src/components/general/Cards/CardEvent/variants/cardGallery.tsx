8a49cd6a8c69765ba265a60be4fbbf00377c2651
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2024-03-28 01:39:49 -0500

    fixed componensts

--- src/components/general/Cards/CardEvent/variants/cardGallery.tsx
+++ src/components/general/Cards/CardEvent/variants/cardGallery.tsx
@@ -1,7 +1,8 @@
-import { Chip, Card, CardBody } from '@nextui-org/react'
+import { Card, CardBody, Image, CardFooter, Button } from '@nextui-org/react'
 import { IconCalendarEvent, IconMapPin, IconClock } from '@tabler/icons-react'
 import { IEvent } from '@/types'
 import { formatDateToDDMMM } from '@/utils/functions'
+import Link from 'next/link'
 
 interface IProps {
   event: IEvent
@@ -12,48 +13,36 @@ export const CardGalleryEvent = (props: IProps) => {
   const { event, showImage = true } = props
   const dateFormatted = formatDateToDDMMM(event.date as string)
   return (
-    <div className="relative shadow-md">
-      <div className="bg-transparent pt-5">
-        <Card
-          className="border px-3 py-4 sm:p-4 lg:px-4 lg:py-9"
-          radius="sm"
-          shadow="sm"
+    <Card
+      className="bg-white max-w-sm"
+      radius="sm"
+      shadow="none"
+    >
+      {showImage && (
+        <Image
+          src={event.banner || 'https://via.placeholder.com/300x200'}
+          alt="Event"
+          removeWrapper
+          className=""
+          radius="none"
+        />
+      )}
+      <CardBody className="px-0">
+        <h3 className="text-sm sm:text-lg xl:text-xl font-bold">
+          {event.name}
+        </h3>
+        <p className="text-sm line-clamp-3">Descripción del evento</p>
+      </CardBody>
+      <CardFooter className="flex justify-end px-0">
+        {/* <Button
+          radius="full"
+          variant="flat"
+          as={Link}
+          href={`/eventos/${event.id}`}
         >
-          <CardBody>
-            <Chip
-              color="primary"
-              size="sm"
-              radius="sm"
-              className="text-white font-bold"
-            >
-              Tag
-            </Chip>
-            <div className="pt-4 space-y-2">
-              <h2 className="text-xl font-bold leading-tight">{event.name}</h2>
-              {/* <p>{date}</p> */}
-              <div className="flex items-center space-x-2">
-                <IconCalendarEvent size={16} />
-                <span className="text-sm">{event.date}</span>
-              </div>
-              <div className="flex items-center space-x-2">
-                <IconClock size={16} />
-                <span className="text-sm">
-                  {event?.timeStart} a {event?.timeEnd}
-                </span>
-              </div>
-              <div className="flex items-center space-x-2">
-                <IconMapPin size={16} />
-                <span className="text-sm">Location</span>
-              </div>
-            </div>
-          </CardBody>
-        </Card>
-      </div>
-      <div className="bg-white px-4 py-2 absolute top-0 left-8 border-t-4 border-primary-400 shadow-md">
-        <div className="font-bold text-lg sm:text-xl lg:text-2xl">
-          {dateFormatted}
-        </div>
-      </div>
-    </div>
+          Ver más
+        </Button> */}
+      </CardFooter>
+    </Card>
   )
 }
