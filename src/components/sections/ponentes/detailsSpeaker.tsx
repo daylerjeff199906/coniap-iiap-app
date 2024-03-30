ed499d64c7767f4353514b59d518f34a4a589f0d
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2024-03-30 14:43:39 -0500

    fixed details speaker

--- src/components/sections/ponentes/detailsSpeaker.tsx
+++ src/components/sections/ponentes/detailsSpeaker.tsx
@@ -1,4 +1,5 @@
 import { IPerson } from '@/types'
+import { Divider } from '@nextui-org/react'
 
 interface IProps {
   speaker: IPerson
@@ -8,11 +9,21 @@ export const DetailsSpeaker = (props: IProps) => {
   const { speaker } = props
   return (
     <>
-      <main>
-        <section>
-          <h1 className="text-2xl sm:text-5xl">
+      <main className="section-home">
+        <section className="space-y-4">
+          <h1 className="text-2xl sm:text-4xl font-bold">
             {speaker?.name} {speaker?.surName}
           </h1>
+          <p className="text-tiny sm:text-medium text-gray-400">
+            {speaker?.job} | {''}
+            <span className="font-bold">{speaker?.institution}</span>
+          </p>
+          <Divider className="w-full max-w-48 py-0.5" />
+          <h3 className="font-medium text-gray-400">{speaker?.location}</h3>
+        </section>
+        <section className="space-y-4">
+          <h2 className="text-xl sm:text-2xl font-bold">Presentación</h2>
+          <p className="text-tiny sm:text-medium">{speaker?.presentation}</p>
         </section>
       </main>
     </>
