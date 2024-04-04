6c44bcf2052a32a186e0f8f59fa1fd6b923280d0
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2024-04-03 22:34:54 -0500

    fixed banner home

--- src/components/sections/home/bannerHome.tsx
+++ src/components/sections/home/bannerHome.tsx
@@ -151,7 +151,7 @@ export const BannerHome = () => {
         className="fixed w-full h-full max-h-screen object-cover  top-0 -z-50"
       >
         <source
-          src="https://firebasestorage.googleapis.com/v0/b/coniap-iiap.appspot.com/o/banners%2Fbg-home.mp4?alt=media&token=2ba28c62-58cc-4a1e-8973-c820b7bc6c6c"
+          src="https://firebasestorage.googleapis.com/v0/b/coniap-iiap.appspot.com/o/banners%2Fbg-home.mp4?alt=media&token=400035af-1dd9-4c63-94f7-02273be31d5a"
           type="video/mp4"
         />
       </video>
