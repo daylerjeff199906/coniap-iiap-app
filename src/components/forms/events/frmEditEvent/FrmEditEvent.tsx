0b200cba2c1c54eb34176fb104ded7c44f2e1714
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2024-04-20 23:54:59 -0500

    fixed edit events

--- src/components/forms/events/frmEditEvent/FrmEditEvent.tsx
+++ src/components/forms/events/frmEditEvent/FrmEditEvent.tsx
@@ -28,7 +28,7 @@ export const FrmEditEvent = (props: IProps) => {
 
   const id = searchParams.get('edit') || ''
 
-  const [isEditables, setIsEditables] = useState(true)
+  // const [isEditables, setIsEditables] = useState(true)
 
   const { updateDataEvent, loading } = useEvents()
 
@@ -67,15 +67,13 @@ export const FrmEditEvent = (props: IProps) => {
     <>
       <FormProvider {...methods}>
         <header>
-          <h2 className="text-2xl font-bold">
-            {isEditables ? 'Editar evento' : 'Detalles de evento'}
-          </h2>
+          <h2 className="text-2xl font-bold">Editar evento: {event?.name}</h2>
         </header>
         <form onSubmit={methods.handleSubmit(onSubmit)}>
           <main className="grid grid-cols-1 gap-6">
             <section className="space-y-2">
               <header className="w-full">
-                <div className="w-full p-2 relative">
+                {/* <div className="w-full p-2 relative">
                   <Switch
                     defaultChecked={isEditables}
                     //   checked={isEditables}
@@ -86,95 +84,42 @@ export const FrmEditEvent = (props: IProps) => {
                       {isEditables ? 'Vizualizar' : 'Editar'}
                     </p>
                   </Switch>
-                </div>
+                </div> */}
                 <HeaderSection />
               </header>
               <div className="space-y-3">
                 <h1 className="text-sm font-semibold">Información general</h1>
-                {isEditables ? (
-                  <>
-                    <div className="grid grid-cols-2 gap-5">
-                      <InfoGeneral />
-                      <MoreInfo />
-                    </div>
-                  </>
-                ) : (
-                  <>
-                    <div>
-                      <h3>Links de acceso al evento</h3>
-                      <div className="flex gap-4">
-                        <Link
-                          href={event?.linkZoom}
-                          target="_blank"
-                          showAnchorIcon
-                        >
-                          Zoom
-                        </Link>
-                        <Link
-                          href={event?.linkYoutube}
-                          target="_blank"
-                          showAnchorIcon
-                        >
-                          Youtube
-                        </Link>
-                        <Link
-                          href={event?.linkFacebook}
-                          target="_blank"
-                          showAnchorIcon
-                        >
-                          Facebook
-                        </Link>
-                      </div>
-                    </div>
-                  </>
-                )}
+                <div className="grid grid-cols-2 gap-5">
+                  <InfoGeneral />
+                  <MoreInfo />
+                </div>
               </div>
               <div className="space-y-3">
                 <h1 className="text-sm font-semibold">
                   Contenido Personalizado
                 </h1>
-                {isEditables ? (
-                  <>
-                    <MoreDescription
-                      defaultContent={event?.customContent ?? ''}
-                    />
-                  </>
-                ) : (
-                  <div className="w-full">
-                    {event?.customContent && (
-                      <div
-                        className="w-full"
-                        dangerouslySetInnerHTML={{
-                          __html: event?.customContent,
-                        }}
-                      />
-                    )}
-                  </div>
-                )}
+                <MoreDescription defaultContent={event?.customContent ?? ''} />
               </div>
             </section>
           </main>
           <footer className="pt-4">
-            {isEditables && (
-              <div className="flex items-center gap-3 justify-end">
-                <Button
-                  color="primary"
-                  isDisabled={loading}
-                  isLoading={loading}
-                  type="submit"
-                >
-                  Guardar cambios
-                </Button>
-                <Button
-                  onPress={() => {
-                    router.push('/admin/eventos')
-                    setIsEditables(true)
-                  }}
-                >
-                  Cancelar
-                </Button>
-              </div>
-            )}
+            <div className="flex items-center gap-3 justify-end">
+              <Button
+                color="primary"
+                isDisabled={loading}
+                isLoading={loading}
+                type="submit"
+              >
+                Guardar cambios
+              </Button>
+              <Button
+                onPress={() => {
+                  router.push('/admin/eventos')
+                }}
+              >
+                Cancelar
+              </Button>
+            </div>
           </footer>
         </form>
       </FormProvider>
