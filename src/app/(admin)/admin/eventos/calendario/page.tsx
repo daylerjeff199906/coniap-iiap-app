808034fc797cbfb24dd942b56c8c279cdab173d0
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2024-04-24 17:44:24 -0500

    resolved errors

--- src/app/(admin)/admin/eventos/calendario/page.tsx
+++ src/app/(admin)/admin/eventos/calendario/page.tsx
@@ -1,5 +1,12 @@
 import { fetchAllEvents } from '@/api'
+import { Calendary } from '@/modules/admin'
+import { IEvent } from '@/types'
 export default async function Page() {
-  const events = await fetchAllEvents('')
-  return <></>
+  const events: IEvent[] = (await fetchAllEvents('')) as unknown as IEvent[]
+
+  return (
+    <>
+      <Calendary data={events} />
+    </>
+  )
 }
--- src/modules/admin/events/components/calendary.tsx
+++ src/modules/admin/events/components/calendary.tsx
@@ -1,3 +1,9 @@
-export const Calendary = () => {
+import { IEvent } from '@/types'
+
+interface IProps {
+  data: Array<IEvent>
+}
+
+export const Calendary = (props: IProps) => {
   return <></>
 }
