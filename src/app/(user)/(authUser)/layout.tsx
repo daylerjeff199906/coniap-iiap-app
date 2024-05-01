ec5aaef4c877b0c4ccee459b386b432bdc727aa9
daylerjeff199906 <daylersan@gmail.com>
2024-05-01 16:12:23 -0500

    fixed auth basic user

--- src/app/(user)/(authUser)/layout.tsx
+++ src/app/(user)/(authUser)/layout.tsx
@@ -1,25 +1,24 @@
 'use client'
 import { BannerStatic } from '@/components'
-import { TabsSections } from '@/modules/user'
-import { AuthProvider } from '@/providers'
+import { TabsSections, AuthProvider } from '@/modules/user'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      {/* <AuthProvider> */}
-      <BannerStatic
-        title="Mi perfil"
-        urlImage=""
-        subtitle="¡Bienvenido!"
-        description="Aquí puedes ver y editar tu perfil."
-      />
-      <section className="container sm:flex gap-4 py-4">
-        <aside className="hidden sm:block">
-          <TabsSections />
-        </aside>
-        <main className="w-full sm:p-4 lg:p-6">{children}</main>
-      </section>
-      {/* </AuthProvider> */}
+      <AuthProvider>
+        <BannerStatic
+          title="Mi perfil"
+          urlImage=""
+          subtitle="¡Bienvenido!"
+          description="Aquí puedes ver y editar tu perfil."
+        />
+        <section className="container sm:flex gap-4 py-4">
+          <aside className="hidden sm:block">
+            <TabsSections />
+          </aside>
+          <main className="w-full sm:p-4 lg:p-6">{children}</main>
+        </section>
+      </AuthProvider>
     </>
   )
 }
--- src/modules/user/provider/authProvider.tsx
+++ src/modules/user/provider/authProvider.tsx
@@ -4,6 +4,12 @@ import { fetchPersonByEmail } from '@/api'
 import { IPerson, IUser } from '@/types'
 import { getCookie } from '@/lib'
 
+interface ICookieRes {
+  name: string
+  path: string
+  value: string
+}
+
 export const AuthContext = createContext<{
   myPerson: IPerson | null
 }>({
@@ -12,18 +18,33 @@ export const AuthContext = createContext<{
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [myPerson, setMyPerson] = useState<IPerson | null>(null)
-  const user: IUser = getCookie('user') as unknown as IUser
+  const [user, setUser] = useState<IUser | null>(null)
+
+  console.log('user', user)
+  console.log('myPerson', myPerson)
+
+  const getUser = async () => {
+    const user: ICookieRes = (await getCookie('user')) as unknown as ICookieRes
+    const dataParse = await JSON.parse(user.value as unknown as string)
+    setUser(dataParse)
+  }
 
   const getPerson = async () => {
-    const res = await fetchPersonByEmail(user.email)
-    if (res) {
-      setMyPerson(res)
+    if (user) {
+      const res = await fetchPersonByEmail(user.email)
+      if (res) {
+        setMyPerson(res)
+      }
     }
   }
 
   useEffect(() => {
-    getPerson()
+    getUser()
   }, [])
+
+  useEffect(() => {
+    getPerson()
+  }, [user])
 
   return (
     <AuthContext.Provider
