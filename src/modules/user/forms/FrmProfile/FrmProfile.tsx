3ba6a18c86401c2ed9730253c0a0aef3176f278e
daylerjeff199906 <daylersan@gmail.com>
2024-05-01 19:13:29 -0500

    fixed frm profile

--- src/modules/user/forms/FrmProfile/FrmProfile.tsx
+++ src/modules/user/forms/FrmProfile/FrmProfile.tsx
@@ -1,20 +1,40 @@
 'use client '
 import { ContactData, PersonData } from './sections'
-import { useForm, FormProvider } from 'react-hook-form'
+import { useForm, FormProvider, SubmitHandler } from 'react-hook-form'
 import { IPerson } from '@/types'
 import { Button } from '@nextui-org/react'
+
+import { usePersons } from '@/hooks/admin'
+import { useAuth } from '../..'
 
 interface IFrmProfileProps {
   person: IPerson
 }
 
 export const FrmProfile = () => {
-  const methods = useForm<IPerson>()
+  const { myPerson } = useAuth()
+  const { updatePersonData, loading } = usePersons()
+
+  const methods = useForm<IPerson>({
+    defaultValues: myPerson || {},
+  })
+
+  const onSubmit: SubmitHandler<IPerson> = async (data: IPerson) => {
+    const res = await updatePersonData(String(myPerson?.id), data)
+    if (res.message) {
+      return null
+    } else {
+      methods.reset()
+    }
+  }
 
   return (
     <>
       <FormProvider {...methods}>
-        <form className="flex flex-col gap-6 max-w-2xl">
+        <form
+          className="flex flex-col gap-6 max-w-2xl"
+          onSubmit={methods.handleSubmit(onSubmit)}
+        >
           <PersonData />
           <ContactData />
 
@@ -22,8 +42,8 @@ export const FrmProfile = () => {
             <div className="flex items-center gap-3 justify-end">
               <Button
                 color="primary"
-                // isDisabled={loading}
-                // isLoading={loading}
+                isDisabled={loading}
+                isLoading={loading}
                 type="submit"
               >
                 Guardar cambios
