2bba8c70c2bdca2753f4e4f94c2a2e0c29c373bb
daylerjeff199906 <daylersan@gmail.com>
2024-06-19 18:36:49 -0500

    fixed details

--- src/components/general/NavBar/variants/navBarAdmin.tsx
+++ src/components/general/NavBar/variants/navBarAdmin.tsx
@@ -16,23 +16,23 @@ import { useRouter } from 'next/navigation'
 import { getLocalStorage, deleteLocalStorage, deleteCookie } from '@/lib'
 import { IUser } from '@/types'
 import { useEffect, useState } from 'react'
-// import { IUser } from '@/types'
+import { useAuthContext } from '@/provider'
 
 export const NavBarAdmin = () => {
-  const [user, setUser] = useState<IUser | null>(null)
+  const { user, logout } = useAuthContext()
+  // const [user, setUser] = useState<IUser | null>(null)
 
   const router = useRouter()
   // const user: IUser = getLocalStorage('user') as unknown as IUser
 
-  useEffect(() => {
-    const user: IUser = getLocalStorage('user')
-    setUser(user)
-  }, [])
+  // useEffect(() => {
+  //   const user: IUser = getLocalStorage('user')
+  //   setUser(user)
+  // }, [])
 
   const handleLogout = async () => {
-    await deleteLocalStorage('user')
-    await deleteCookie('user')
-    router.refresh()
+    logout()
+    router.push('/')
   }
 
   return (
