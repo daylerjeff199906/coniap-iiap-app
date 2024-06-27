250af5832ecdf8b732157b02b9f8a0ead8aa6df2
daylerjeff199906 <daylersan@gmail.com>
2024-06-26 20:35:04 -0500

    feat: Agregar soporte para filtrar personas por estado en la búsqueda

--- src/modules/admin/participants/pages/ListParticipants/sections/FiltersSection.tsx
+++ src/modules/admin/participants/pages/ListParticipants/sections/FiltersSection.tsx
@@ -9,10 +9,17 @@ const personsType = [
   { value: 'participant', label: 'Asistente' },
 ]
 
+const activeStatus = [
+  { value: 'all', label: 'Todos' },
+  { value: 'active', label: 'Activo' },
+  { value: 'inactive', label: 'Inactivo' },
+]
+
 export const FiltersSection = () => {
   const { getParams, updateFilter } = useFilterFromUrl()
 
   const selectedTypePerson = getParams('typePerson', 'all')
+  const selectedStatus = getParams('status', 'all')
 
   const handleTypePerson = (val: Selection) => {
     const value = Object.values(val)[0]
@@ -23,9 +30,18 @@ export const FiltersSection = () => {
     }
   }
 
+  const handleStatus = (val: Selection) => {
+    const value = Object.values(val)[0]
+    if (value === 'all') {
+      updateFilter('status', '')
+    } else {
+      updateFilter('status', value)
+    }
+  }
+
   return (
     <>
-      <div className="flex gap-2 w-full max-w-[180px]">
+      <div className="flex gap-2 w-full max-w-[280px]">
         <Select
           aria-label="Tipo de persona"
           aria-labelledby="Tipo de persona"
@@ -34,6 +50,7 @@ export const FiltersSection = () => {
           selectedKeys={[selectedTypePerson]}
           onSelectionChange={(value) => handleTypePerson(value)}
           disallowEmptySelection
+          label="Tipo de persona"
         >
           {personsType.map((type, i) => (
             <SelectItem
@@ -43,6 +60,29 @@ export const FiltersSection = () => {
               value={type.value}
             >
               {type.label}
+            </SelectItem>
+          ))}
+        </Select>
+      </div>
+      <div className="flex gap-2 w-full max-w-[210px]">
+        <Select
+          aria-label="Estado"
+          aria-labelledby="Estado"
+          radius="sm"
+          variant="bordered"
+          selectedKeys={[selectedStatus]}
+          onSelectionChange={(value) => handleStatus(value)}
+          disallowEmptySelection
+          label="Estado"
+        >
+          {activeStatus.map((status, i) => (
+            <SelectItem
+              aria-label={`Estado ${status.label}`}
+              aria-labelledby={`Estado ${status.label}`}
+              key={status.value}
+              value={status.value}
+            >
+              {status.label}
             </SelectItem>
           ))}
         </Select>
