41a2e717eb88b88132cc3289fe5371d63d6bf8cb
daylerjeff199906 <daylersan@gmail.com>
2024-06-26 21:34:08 -0500

    feat: Actualizar página de detalles del participante para mostrar resúmenes adjuntos

--- src/app/(admin)/admin/participantes/ponentes/[id]/page.tsx
+++ src/app/(admin)/admin/participantes/ponentes/[id]/page.tsx
@@ -1,23 +1,44 @@
-import { fetchPersonById, fetchSummaryById } from '@/api'
+import { fetchPersonById, fetchSummaryByIdPerson } from '@/api'
 import { DetailsParticipant } from '@/modules/admin'
+import { IPerson, ISummary } from '@/types'
 
 interface IProps {
   params: {
-    type: string
     id: string
   }
 }
-export default async function Page(props: IProps) {
-  const { params } = props
 
-  const person =
-    params.type !== 'resumenes'
-      ? await fetchPersonById(params.id)
-      : await fetchSummaryById(params.id)
+export default async function Page(props: IProps) {
+  const { id } = props.params
+
+  const person: IPerson = await fetchPersonById(id)
+    .then((res) => res)
+    .catch((err) => err)
+
+  const summaries: ISummary[] | null = await fetchSummaryByIdPerson(id)
+    .then((res) => res)
+    .catch((err) => err)
+
+  if (person) {
+    return (
+      <main className="w-full flex flex-col justify-center items-center">
+        <section className="flex flex-col w-full justify-center items-center max-w-5xl">
+          <DetailsParticipant
+            data={person}
+            summaries={summaries}
+          />
+        </section>
+      </main>
+    )
+  }
 
   return (
-    <div className="grid grid-cols-1 gap-4">
-      <DetailsParticipant data={person} />
-    </div>
+    <>
+      <main className="w-full flex flex-col justify-center items-center">
+        <section className="flex flex-col justify-center items-center max-w-5xl p-6 border rounded-xl bg-white shadow-md">
+          <h1 className="text-xl font-bold">No se encontro el participante</h1>
+        </section>
+      </main>
+    </>
   )
 }
