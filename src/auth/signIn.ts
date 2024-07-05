7af122a207de921825f49ea803e9b03df00d7a4c
daylerjeff199906 <daylersan@gmail.com>
2024-07-05 18:22:55 -0500

    fixed details

--- src/auth/signIn.ts
+++ src/auth/signIn.ts
@@ -1,12 +1,12 @@
 'use client'
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
-import {
-  getFirestore,
-  collection,
-  query,
-  where,
-  getDocs,
-} from 'firebase/firestore'
+// import {
+//   getFirestore,
+//   collection,
+//   query,
+//   where,
+//   getDocs,
+// } from 'firebase/firestore'
 import { fetchUserByEmail } from '@/api'
 import { IError } from './types'
 
@@ -24,7 +24,6 @@ export const signInWithCredentials = async (
   props: ILogin
 ): Promise<IUser | null> => {
   const auth = getAuth()
-  const firestore = getFirestore()
   const { email, password } = props
 
   try {
@@ -34,37 +33,12 @@ export const signInWithCredentials = async (
       password
     )
     if (userCredential.user.emailVerified) {
-      // //Busco en la tabla users si existe el usuario
-      // const usersRef = collection(firestore, 'users')
-      // const q = query(usersRef, where('email', '==', userCredential.user.email))
-      // const querySnapshot = await getDocs(q)
-
       //Se busca si existe la persona en la tabla person
       const user: IUser | null = (await fetchUserByEmail(
         userCredential.user.email as string
       )) as IUser | null
 
-      let userData: IUser | null = null
-      // if (querySnapshot.empty) {
-      //   userData = {
-      //     id: person.id as string,
-      //     userName: person.name || '',
-      //     email: userCredential.user.email || '',
-      //     photo: person.image || '',
-      //     role: null,
-      //     person: person,
-      //   }
-      // } else {
-      //   userData = {
-      //     id: querySnapshot.docs[0].id,
-      //     userName: userCredential.user.displayName || '',
-      //     email: userCredential.user.email || '',
-      //     photo: userCredential.user.photoURL || '',
-      //     role: querySnapshot.docs[0].data().role || '',
-      //     person: person,
-      //   }
-      // }
-      return userData
+      return user
     } else {
       console.log(userCredential.user.emailVerified)
       toast.error('Revise su correo electrónico para verificar su cuenta', {
--- src/auth/signInWithGoogle.ts
+++ src/auth/signInWithGoogle.ts
@@ -1,18 +1,18 @@
 'use client'
 import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
 import { auth } from '@/firebase/firebase'
-import {
-  getFirestore,
-  collection,
-  query,
-  where,
-  getDocs,
-} from 'firebase/firestore'
+// import {
+//   getFirestore,
+//   collection,
+//   query,
+//   where,
+//   getDocs,
+// } from 'firebase/firestore'
 import { IPerson, IUser } from '@/types'
 import { IError } from './types'
 import { toast } from 'sonner'
 
-import { fetchPersonByEmail } from '@/api'
+import { fetchUserByEmail } from '@/api'
 import { getErrors } from './getErrors'
 
 export const SignInWithGoogle = async (): Promise<IUser | null> => {
@@ -22,37 +22,12 @@ export const SignInWithGoogle = async (): Promise<IUser | null> => {
     const result = await signInWithPopup(auth, provider)
     const user = result.user
 
-    const firestore = getFirestore()
-    const usersRef = collection(firestore, 'users')
-    const q = query(usersRef, where('email', '==', user.email))
-    const querySnapshot = await getDocs(q)
-
     //Se busca si existe la persona en la tabla person
-    const person: IPerson = (await fetchPersonByEmail(
+    const userApi: IUser | null = (await fetchUserByEmail(
       user.email as string
-    )) as IPerson
+    )) as IUser | null
 
-    let userData: IUser | null = null
-    if (querySnapshot.empty) {
-      userData = {
-        id: person.id as string,
-        userName: person.name || '',
-        email: user.email || '',
-        photo: person.image || '',
-        role: null,
-        person: person,
-      }
-    } else {
-      userData = {
-        id: querySnapshot.docs[0].id,
-        userName: user.displayName || '',
-        email: user.email || '',
-        photo: user.photoURL || '',
-        role: querySnapshot.docs[0].data().role || '',
-        person: person,
-      }
-    }
-    return userData
+    return userApi
   } catch (error) {
     const err = error as unknown as IError
     toast.error(getErrors(err))
