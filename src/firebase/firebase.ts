bbabee92ba8508bbceb17f977b857db5bcb94d56
daylerjeff199906 <daylersan@gmail.com>
2024-07-06 01:06:48 -0500

    protected keys

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -5,19 +5,25 @@ import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 // add firebase auth
 import { getAuth } from 'firebase/auth'
+
+// IMPORT VARIABLES FROM .ENV
+const API_KEY = process.env.VITE_FIREBASE_API_KEY
+const MESSAGE_SENDER_ID = process.env.VITE_FIREBASE_MESSAGING_SENDER_ID
+const APP_ID = process.env.VITE_FIREBASE_APP_ID
+const PROJECT_ID = process.env.VITE_FIREBASE_MEASUREMENT_ID
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyAl-3I574JF14zfaCgtkE6U6OrGsYQb9_c',
+  apiKey: API_KEY,
   authDomain: 'coniap-iiap.firebaseapp.com',
   projectId: 'coniap-iiap',
   storageBucket: 'coniap-iiap.appspot.com',
-  messagingSenderId: '443032108527',
-  appId: '1:443032108527:web:e810f87fb40fc5ad028a1a',
-  measurementId: 'G-Z4R4EY00Y1',
+  messagingSenderId: MESSAGE_SENDER_ID,
+  appId: APP_ID,
+  measurementId: PROJECT_ID,
 }
 
 // Initialize Firebase
