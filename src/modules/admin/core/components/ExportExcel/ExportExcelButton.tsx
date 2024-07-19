5227032e1439cb4978aae9bce283b97a02de3d5a
daylerjeff199906 <daylersan@gmail.com>
2024-07-19 14:49:15 -0500

    fix: change save file

--- src/modules/admin/core/components/ExportExcel/ExportExcelButton.tsx
+++ src/modules/admin/core/components/ExportExcel/ExportExcelButton.tsx
@@ -1,5 +1,5 @@
 'use client'
-import * as XLSX from 'xlsx'
+import ExcelJS from 'exceljs'
 import { saveAs } from 'file-saver'
 import { Button } from '@nextui-org/react'
 import { IconDownload } from '@tabler/icons-react'
@@ -17,19 +17,25 @@ export const ExportExcelButton = <T extends Record<string, any>>(
   const name = fileName || 'data.xlsx'
 
   const handleExportToExcel = () => {
-    // Crear una hoja de trabajo de Excel (worksheet) a partir de los datos
-    const worksheet = XLSX.utils.json_to_sheet(dataList)
-    // Crear un libro de trabajo (workbook) y agregar la hoja de trabajo
-    const workbook = XLSX.utils.book_new()
-    XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1')
-    // Generar un archivo Excel binario
-    const excelBuffer = XLSX.write(workbook, {
-      bookType: 'xlsx',
-      type: 'array',
+    const workbook = new ExcelJS.Workbook()
+    const worksheet = workbook.addWorksheet('Sheet 1')
+
+    // Agregar las cabeceras
+    const headers = Object.keys(dataList[0])
+
+    worksheet.addRow(headers)
+
+    // Agregar los datos
+    dataList.forEach((data) => {
+      const row = headers.map((header) => data[header])
+      worksheet.addRow(row)
     })
+
     // Guardar el archivo con file-saver
-    const data = new Blob([excelBuffer], { type: 'application/octet-stream' })
-    saveAs(data, name)
+    workbook.xlsx.writeBuffer().then((buffer) => {
+      const data = new Blob([buffer], { type: 'application/octet-stream' })
+      saveAs(data, name)
+    })
   }
 
   return (
