f6629201fed489ce44ab1a898582509efe4073e1
daylerjeff199906 <daylersan@gmail.com>
2024-07-19 19:40:55 -0500

    fix: fix toas to use programs

--- src/hooks/admin/usePrograms.ts
+++ src/hooks/admin/usePrograms.ts
@@ -33,9 +33,7 @@ export function usePrograms() {
     const res: IRes = (await createProgram(data)) as IRes
 
     if (res.message) {
-      toast.error('Error al crear el programa', {
-        description: res.message,
-      })
+      toast.error(`Error al crear el programa: ${res.message}`)
     } else {
       toast.success('Programa creado con exito')
     }
@@ -47,9 +45,7 @@ export function usePrograms() {
     setLoading(true)
     const res: IRes = (await updateProgram(id, data)) as IRes
     if (res.message) {
-      toast.error('Error al actualizar el programa', {
-        description: res.message,
-      })
+      toast.error(`Error al actualizar el programa: ${res.message}`)
     } else {
       toast.success('Programa actualizado con exito')
     }
