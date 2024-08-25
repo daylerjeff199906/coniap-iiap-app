4b1820cc86ba6b470c5b7aa029ececc48c723087
daylerjeff199906 <daylersan@gmail.com>
2024-08-25 13:01:50 -0500

    feat: add modal aviso in home

--- src/modules/core/components/ModalAviso/ModalAviso.tsx
+++ src/modules/core/components/ModalAviso/ModalAviso.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Modal, ModalBody, ModalContent, ModalFooter } from '@nextui-org/react'
+import { Modal, ModalBody, ModalContent } from '@nextui-org/react'
 
 interface IProps {
   isOpen: boolean
@@ -14,11 +14,42 @@ export const ModalAviso = (props: IProps) => {
       isOpen={isOpen}
       onClose={() => setIsOpen(false)}
       radius="sm"
-      size="4xl"
+      size="2xl"
     >
       <ModalContent>
-        <ModalBody></ModalBody>
-        <ModalFooter></ModalFooter>
+        <ModalBody>
+          <div
+            style={{
+              position: 'relative',
+              width: '100%',
+              height: '0',
+              paddingTop: '100%',
+              paddingBottom: '0',
+              boxShadow: '0 2px 8px 0 rgba(63,69,81,0.16)',
+              marginTop: '1.6em',
+              marginBottom: '0.9em',
+              overflow: 'hidden',
+              borderRadius: '8px',
+              willChange: 'transform',
+            }}
+          >
+            <iframe
+              loading="lazy"
+              src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGO4X-Wvxg&#x2F;G3Z2j3L8qy0mgbPAqNGoKg&#x2F;view?embed"
+              allow="fullscreen"
+              style={{
+                position: 'absolute',
+                width: '100%',
+                height: '100%',
+                top: '0',
+                left: '0',
+                border: 'none',
+                padding: '0',
+                margin: '0',
+              }}
+            ></iframe>
+          </div>
+        </ModalBody>
       </ModalContent>
     </Modal>
   )
