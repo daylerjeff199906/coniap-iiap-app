cf738bcaa0569385a9cc4ceb4f57a1d78cbc6066
daylerjeff199906 <daylersan@gmail.com>
2024-08-30 22:23:43 -0500

    feat: add filters in the reports summary

--- src/modules/admin/reports/pages/ParticipantsReports/FilterSection.tsx
+++ src/modules/admin/reports/pages/ParticipantsReports/FilterSection.tsx
@@ -1,116 +1,108 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 'use client'
 import { useFilterFromUrl } from '@/modules/core'
-import { Button, Select, SelectItem, Selection } from '@nextui-org/react'
+import {
+  Accordion,
+  AccordionItem,
+  Badge,
+  Button,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@nextui-org/react'
 import { IconFilter } from '@tabler/icons-react'
 
-const personsType = [
-  { value: 'all', label: 'Todos' },
-  { value: 'speaker', label: 'Ponente' },
-  { value: 'speaker_mg', label: 'Ponente Magistral' },
-  { value: 'participant', label: 'Asistente' },
-]
+import {
+  AprovedFiltered,
+  StatusFilter,
+  FileFiltered,
+  TopicsFiltered,
+} from '@/modules/admin/summaries/components'
 
-const activeStatus = [
-  { value: 'all', label: 'Todos' },
-  { value: 'active', label: 'Activo' },
-  { value: 'inactive', label: 'Inactivo' },
-]
+export const FiltersSection = () => {
+  const { filteredParams } = useFilterFromUrl()
 
-interface IProps {
-  onChageFilter: () => void
-  onClearFilter: () => void
-}
+  const filteredList = [
+    {
+      key: 'status',
+      name: 'Estado',
+      items: <StatusFilter />,
+    },
+    {
+      key: 'aproved',
+      name: 'Aprobado',
+      items: <AprovedFiltered />,
+    },
+    {
+      key: 'file',
+      name: 'Tiene archivo',
+      items: <FileFiltered />,
+    },
+    {
+      key: 'topic',
+      name: 'Tema',
+      items: <TopicsFiltered />,
+    },
+  ]
 
-export const FiltersSection = (props: IProps) => {
-  const { getParams, updateFilter } = useFilterFromUrl()
-  const { onChageFilter, onClearFilter } = props
+  const filtersLabel = [
+    { value: 'topic', name: 'Tema' },
+    { value: 'aproved', name: 'Aprobado' },
+    { value: 'status', name: 'Estado' },
+    { value: 'file', name: 'Tiene archivo' },
+    { value: 'date', name: 'Fecha' },
+  ]
 
-  const selectedTypePerson = getParams('typePerson', 'all')
-  const selectedStatus = getParams('status', 'all')
-
-  const handleTypePerson = (val: Selection) => {
-    const value = Object.values(val)[0]
-    if (value === 'all') {
-      updateFilter('typePerson', '')
-    } else {
-      updateFilter('typePerson', value)
-    }
-  }
-
-  const handleStatus = (val: Selection) => {
-    const value = Object.values(val)[0]
-    if (value === 'all') {
-      updateFilter('status', '')
-    } else {
-      updateFilter('status', value)
-    }
-  }
+  const selectedFilter = filteredParams(filtersLabel)
 
   return (
     <>
-      <div className="flex gap-2 w-full sm:max-w-[210px]">
-        <Select
-          aria-label="Tipo de persona"
-          aria-labelledby="Tipo de persona"
-          radius="sm"
-          variant="bordered"
-          selectedKeys={[selectedTypePerson]}
-          onSelectionChange={(value) => handleTypePerson(value)}
-          disallowEmptySelection
-          description="Tipo de persona"
-        >
-          {personsType.map((type, i) => (
-            <SelectItem
-              aria-label={`Tipo de persona ${type.label}`}
-              aria-labelledby={`Tipo de persona ${type.label}`}
-              key={type.value}
-              value={type.value}
-            >
-              {type.label}
-            </SelectItem>
-          ))}
-        </Select>
-      </div>
-      <div className="flex gap-2 w-full sm:max-w-[210px]">
-        <Select
-          aria-label="Estado"
-          aria-labelledby="Estado"
-          radius="sm"
-          variant="bordered"
-          selectedKeys={[selectedStatus]}
-          onSelectionChange={(value) => handleStatus(value)}
-          disallowEmptySelection
-          description="Estado de la persona"
-        >
-          {activeStatus.map((status, i) => (
-            <SelectItem
-              aria-label={`Estado ${status.label}`}
-              aria-labelledby={`Estado ${status.label}`}
-              key={status.value}
-              value={status.value}
-            >
-              {status.label}
-            </SelectItem>
-          ))}
-        </Select>
-      </div>
-      <Button
+      <Popover
+        placement="right-start"
+        size="sm"
         radius="sm"
-        className="button-dark"
-        startContent={<IconFilter size={20} />}
-        onPress={onChageFilter}
       >
-        Filtrar
-      </Button>
-      {selectedTypePerson !== 'all' || selectedStatus !== 'all' ? (
-        <Button
-          radius="sm"
-          onPress={onClearFilter}
-          color="warning"
-        >
-          Limpiar
-        </Button>
-      ) : null}
+        <PopoverTrigger>
+          <Button
+            radius="sm"
+            startContent={
+              <Badge
+                content={selectedFilter?.length}
+                isInvisible={selectedFilter?.length === 0}
+                color="danger"
+              >
+                <IconFilter
+                  size={18}
+                  stroke={1.5}
+                />
+              </Badge>
+            }
+            className="font-semibold"
+          >
+            Filtros
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent>
+          <main className=" w-64">
+            <Accordion
+              isCompact
+              defaultExpandedKeys={selectedFilter?.map(
+                (filter) => filter.value
+              )}
+            >
+              {filteredList.map((filter) => (
+                <AccordionItem
+                  key={filter.key}
+                  aria-label={`Filter by ${filter.key}`}
+                  title={filter.name}
+                >
+                  {filter.items}
+                </AccordionItem>
+              ))}
+            </Accordion>
+          </main>
+        </PopoverContent>
+      </Popover>
     </>
   )
 }
