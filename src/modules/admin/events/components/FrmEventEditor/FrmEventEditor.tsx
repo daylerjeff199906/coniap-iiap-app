ec5f9d36e826fcd3020bc3f59d0bb0218a7acfc5
daylerjeff199906 <daylersan@gmail.com>
2024-09-14 07:13:43 -0500

    Refactorizar FrmEventEditor.tsx eliminando console.log

--- src/modules/admin/events/components/FrmEventEditor/FrmEventEditor.tsx
+++ src/modules/admin/events/components/FrmEventEditor/FrmEventEditor.tsx
@@ -62,7 +62,6 @@ export const FrmEventEditor = (props: IProps) => {
       sala_name,
       ...resData
     } = data
-    console.log('data', data)
 
     const fileIsArray = Array.isArray(file)
     let newData: IEventRes = {
