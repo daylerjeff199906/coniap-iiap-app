a53ab76d20cfe87fa3ae1f7daab3756bb8f1f5f9
daylerjeff199906 <daylersan@gmail.com>
2024-09-14 01:57:25 -0500

    feat: add table editor text

--- src/modules/admin/core/components/index.ts
+++ src/modules/admin/core/components/index.ts
@@ -2,5 +2,14 @@ import { DialogStatus } from './DialogStatus/DialogStatus'
 import { ModalRender } from './ModalRender/ModalRender'
 import { AsideMenu } from './AsideMenu/AsideMenu'
 import { ExportExcelButton } from './ExportExcel/ExportExcelButton'
+import { ForwardRefEditor } from './InitializedMDXEditor/ForwardRefEditor'
+import InitializedMDXEditor from './InitializedMDXEditor/InitializedMDXEditor'
 
-export { DialogStatus, ModalRender, AsideMenu, ExportExcelButton }
+export {
+  DialogStatus,
+  ModalRender,
+  AsideMenu,
+  ExportExcelButton,
+  ForwardRefEditor,
+  InitializedMDXEditor,
+}
--- src/modules/admin/events/components/FrmEventEditor/sections/moreDescription.tsx
+++ src/modules/admin/events/components/FrmEventEditor/sections/moreDescription.tsx
@@ -1,36 +1,67 @@
 'use client'
+import '@mdxeditor/editor/style.css'
 import { Controller, useFormContext } from 'react-hook-form'
-import { Skeleton } from '@nextui-org/react'
-import dynamic from 'next/dynamic'
+// import { Skeleton } from '@nextui-org/react'
+// import dynamic from 'next/dynamic'
 
 //For the text field
-const ReactQuill = dynamic(() => import('react-quill'), {
-  ssr: false,
-  loading: () => (
-    <div className="w-ful">
-      <Skeleton className="max-h-52 w-full h-52 rounded-md" />
-    </div>
-  ),
-})
+// const ReactQuill = dynamic(() => import('react-quill'), {
+//   ssr: false,
+//   loading: () => (
+//     <div className="w-ful">
+//       <Skeleton className="max-h-52 w-full h-52 rounded-md" />
+//     </div>
+//   ),
+// })
 import 'react-quill/dist/quill.snow.css'
 import { IEvent } from '@/types'
+import { useRef } from 'react'
+import {
+  BoldItalicUnderlineToggles,
+  headingsPlugin,
+  InsertTable,
+  MDXEditor,
+  MDXEditorMethods,
+  quotePlugin,
+  thematicBreakPlugin,
+  toolbarPlugin,
+  UndoRedo,
+  tablePlugin,
+} from '@mdxeditor/editor'
 
 export const MoreDescription = () => {
   const { control } = useFormContext<IEvent>()
+  const ref = useRef<MDXEditorMethods>(null)
 
   return (
     <section className="grid grid-cols-1 gap-4">
       <div className=" h-72 section-admin">
         <h1 className="text-gray-400">Personalizar contenido</h1>
+
         <Controller
           control={control}
           name="customContent"
           render={({ field: { value, onChange } }) => (
-            <ReactQuill
-              value={value}
-              onChange={onChange}
-              theme="snow"
-              className="max-h-52 w-full h-48"
+            <MDXEditor
+              ref={ref}
+              plugins={[
+                toolbarPlugin({
+                  toolbarContents: () => (
+                    <>
+                      <UndoRedo />
+                      <BoldItalicUnderlineToggles />
+                      <InsertTable /> {/* Plugin para insertar tablas */}
+                      {/* Plugin para cambiar tamaño de fuente */}
+                    </>
+                  ),
+                }),
+                headingsPlugin(), // Plugin para manejar encabezados
+                thematicBreakPlugin(), // Línea horizontal
+                quotePlugin(), // Citas
+                tablePlugin(),
+              ]}
+              markdown={value || ''}
+              onChange={(value) => onChange(value)}
             />
           )}
         />
