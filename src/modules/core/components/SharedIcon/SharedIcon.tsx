0020863b64f84d94b7dcdabca1b140d4a2270b95
daylerjeff199906 <daylersan@gmail.com>
2024-09-14 00:28:23 -0500

    feat: add SharedIcon component and validateUrlBase functions
    This commit adds a new component called SharedIcon, which is responsible for rendering social media icons for sharing content. It also includes two utility functions, validateUrlBase and getAppName, which are used to retrieve the base URL and application name respectively.
    
    These changes enhance the functionality and improve the reusability of the codebase.

---
+++ src/modules/core/components/SharedIcon/SharedIcon.tsx
@@ -0,0 +1,70 @@
+'use client'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+const socialMedia = {
+  facebook: {
+    url: 'https://www.facebook.com/sharer/sharer.php?u=',
+    icon: 'facebook',
+    name: 'Facebook',
+  },
+  whatsapp: {
+    url: 'https://api.whatsapp.com/send?text=',
+    icon: 'whatsApp',
+    name: 'WhatsApp',
+  },
+  linkedin: {
+    url: 'https://www.linkedin.com/shareArticle?mini=true&url=',
+    icon: 'linkedin',
+    name: 'LinkedIn',
+  },
+  x: {
+    url: 'https://twitter.com/intent/tweet?url=',
+    icon: 'x',
+    name: 'Twitter',
+  },
+  instagram: {
+    url: 'https://www.instagram.com/',
+    icon: 'instagram',
+    name: 'Instagram',
+  },
+}
+
+interface IProps {
+  social: 'facebook' | 'whatsapp' | 'linkedin' | 'x' | 'instagram'
+  size?: 'xs' | 'sm' | 'md' | 'lg'
+}
+
+export const ShareIcons = (props: IProps) => {
+  const { social } = props
+  const pathname = usePathname()
+
+  const size = {
+    xs: 16,
+    sm: 24,
+    md: 28,
+    lg: 32,
+  }
+
+  const sizeSelected = size[props.size || 'md']
+  const socialSelected = socialMedia[social]
+
+  return (
+    <div id="share-media">
+      <Link
+        href={`${socialSelected.url}${pathname}`}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex items-center gap-2 hover:opacity-80"
+      >
+        <Image
+          src={`/social-network/${socialSelected.icon}.svg`}
+          alt={socialSelected.name}
+          width={sizeSelected}
+          height={sizeSelected}
+        />
+      </Link>
+    </div>
+  )
+}
