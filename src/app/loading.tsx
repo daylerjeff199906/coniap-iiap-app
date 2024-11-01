45e03c9e6910c54e6ff2ad59e5cd8abfbd7957d7
daylerjeff199906 <daylersan@gmail.com>
2024-11-01 01:47:11 -0500

    feat: fixed img tag

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,15 +1,13 @@
-import Image from 'next/image'
 export default function Loading() {
   return (
     <>
       <main className="h-screen flex justify-center items-center bg-gray-100">
         <section className="flex flex-col items-center">
-          <Image
+          <img
             src="/logo_coniap.gif"
             width={420}
             height={420}
             alt="Loading"
-            unoptimized={true}
           />
         </section>
       </main>
