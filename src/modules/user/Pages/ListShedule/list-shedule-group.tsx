48d199c6494878be64a68dbddf66356cfe7d1fa1
daylerjeff199906 <daylersan@gmail.com>
2024-11-13 06:27:45 -0500

    feat: fixed bg

--- src/modules/user/Pages/ListShedule/list-shedule-group.tsx
+++ src/modules/user/Pages/ListShedule/list-shedule-group.tsx
@@ -25,7 +25,7 @@ export const ListSheduleGroup = (props: IProps) => {
           className="mb-6"
         >
           <div className="flex flex-col sm:flex-row gap-3 items-start border-b ">
-            <div className="text-xl font-bold pr-4 sm:pr-6 lg:pr-8 w-full sm:min-w-[160px] sm:w-[160px] sticky top-20 z-10 bg-white">
+            <div className="text-xl font-bold pr-4 sm:pr-6 lg:pr-8 w-full sm:min-w-[160px] sm:w-[160px] sticky top-20 z-10 bg-white sm:bg-transparent">
               {hour}
             </div>
             <div
