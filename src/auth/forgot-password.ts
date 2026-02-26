3ae91d1c38900d6f138f521ee90f2663bd43f163
unknown <UNAP@economicoslap05.postgradounap.edu.pe>
2026-02-26 08:12:44 -0500

    fixed data

--- src/auth/forgot-password.ts
+++ src/auth/forgot-password.ts
@@ -1,9 +1,8 @@
 import { getAuth, sendPasswordResetEmail, updateProfile } from 'firebase/auth'
-import { auth } from '@/firebase/firebase'
 
 export async function sendResetPasswordEmail(email: string) {
   try {
-    await sendPasswordResetEmail(auth, email)
+    // await sendPasswordResetEmail(auth, email)
     return 'Email sent'
   } catch (error) {
     return error
