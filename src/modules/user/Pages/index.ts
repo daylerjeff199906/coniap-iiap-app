d544e2e8e1e518f1ea653e5d5e2893725efffbb8
unknown <UNAP@economicoslap05.postgradounap.edu.pe>
2026-02-27 12:32:38 -0500

    fixed details

--- src/modules/user/Pages/index.ts
+++ src/modules/user/Pages/index.ts
@@ -1,5 +1,4 @@
 import { ListShedule } from './ListShedule/ListShedule'
-import { ListSummaries } from './ListSummaries/ListSummaries'
 import { NextSteps } from './NextSteps/NextSteps'
 import { ListSpeakers } from './ListSpeakers/ListSpeakers'
 import { ListMastersSpeakers } from './ListSpeakers/ListMastersSpeakers'
@@ -15,7 +14,6 @@ import { DetailsEvent } from './DetailsEvent/DetailsEvent'
 
 export {
   ListShedule,
-  ListSummaries,
   NextSteps,
   ListMastersSpeakers,
   ListSpeakers,
