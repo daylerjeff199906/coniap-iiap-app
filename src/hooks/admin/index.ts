68554d6adc68f5e9815c8c9a3b933b8f626d3885
DESKTOP-DCPA18G\Dayler <daylersan@gmail.com>
2026-03-09 19:14:23 -0500

    fixed add module participants

--- src/hooks/admin/index.ts
+++ src/hooks/admin/index.ts
@@ -4,7 +4,6 @@ import { useSpeakers } from './useSpeaker'
 import { useFiles } from './useFiles'
 import { useSponsors } from './useSponsors'
 import { useTopics } from './useTopics'
-import { usePersons } from './usePersons'
 import { useCountries } from './useCountries'
 import { useSummaries } from './useSumaries'
 import { useSalas } from './useSalas'
@@ -17,7 +16,6 @@ export {
   useFiles,
   useSponsors,
   useTopics,
-  usePersons,
   useCountries,
   useSummaries,
   useSalas,
